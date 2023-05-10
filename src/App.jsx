081584ed951646ff0de28a85fba7b44469b7fc16
badr ghanbi <bghanbi50@gmail.com>
2023-05-10 09:43:14 +0100

    add the colors and create the base core of the form

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen   flex items-center justify-center bg-Offwhite' >
+    <div className='bg-White w-[70%] h-[400px] p-4 lg:w-1/2  '>
+      {/* header */}
+      <form className='flex justify-between '>
+        <div>
+          <label  className='flex text-Smokeygrey font-bold uppercase text-sm'  for="day">day</label>
+                <input className='w-20 border-[1px]  py-2' id="day" type='number' value="30" name="day" />
+        </div>
+        <div>
+          <label className='flex font-bold uppercase text-Smokeygrey text-sm' for="month" >month</label>
+                <input className=' w-20 border-[1px]  py-2' id="month" type='number' value="11" name="month" />
+        </div>
+        <div>
+          <label className='flex font-bold uppercase text-Smokeygrey text-sm'  for="year" >year</label>
+                <input className=' w-20 border-[1px]  py-2' id="day" type='number' value="2020" name="year" />
+        </div>
+      </form>
+    </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
+    colors:{
+      Purple: 'hsl(259, 100%, 65%)',
+      Lightred: 'hsl(0, 100%, 67%)',
+      White: 'hsl(0, 0%, 100%)',
+      Offwhite: 'hsl(0, 0%, 94%)',
+      Lightgrey: 'hsl(0, 0%, 86%)',
+      Smokeygrey: 'hsl(0, 1%, 44%)',
+      Offblack: 'hsl(0, 0%, 8%)',
+    },
     extend: {},
   },
   plugins: [],
