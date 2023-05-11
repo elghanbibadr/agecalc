2cb043153a4fd5d4ce0639bbe77f7c872295f33d
badr ghanbi <bghanbi50@gmail.com>
2023-05-11 10:51:49 +0100

    add the submit arrow icon of the input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import iconArrow from "../src/assets/images/icon-arrow.svg"
 
 const App = () => {
   const [day, setDay] = useState('30');
@@ -10,41 +11,46 @@ const App = () => {
   const handleYearChange = (e) => setYear(e.target.value)
 
   return (
-    <div className='h-screen   flex items-center justify-center bg-Offwhite' >
-      <div className='bg-White w-[70%] h-[400px] p-4 lg:w-1/2  '>
-        {/* header */}
-        <form className='flex justify-between '>
-          <div>
-            <label htmlFor="day">day</label>
-            <input id="day"
-              type='number'
-              value={day}
-              name="day"
-              onChange={handleDayChange}
-              placeholder='DD' />
-          </div>
-          <div>
-            <label htmlFor="month" >month</label>
-            <input id="month"
-            className="appearance-none"
-              type='number'
-              value={month}
-              onChange={handleMonthChange}
-              name="month"
-              placeholder='MM' />
-          </div>
-          <div>
-            <label htmlFor="year" >year</label>
-            <input id="day"
-              type='number'
-              value={year}
-              onChange={handleYearChange}
-              name="year"
-              placeholder='YYYY' />
-          </div>
-        </form>
+    <div className='h-screen  flex justify-center items-center   bg-Offwhite' >
+        <div className='bg-White w-[70%] h-[400px] p-4 lg:w-1/2  '>
+          {/* header */}
+          <form className='flex flex-col justify-between '>
+      <div className="flex items-center justify-between">
+            <div>
+              <label htmlFor="day">day</label>
+              <input id="day"
+                type='number'
+                value={day}
+                name="day"
+                onChange={handleDayChange}
+                placeholder='DD' />
+            </div>
+            <div>
+              <label htmlFor="month" >month</label>
+              <input id="month"
+              className="appearance-none"
+                type='number'
+                value={month}
+                onChange={handleMonthChange}
+                name="month"
+                placeholder='MM' />
+            </div>
+            <div>
+              <label htmlFor="year" >year</label>
+              <input id="day"
+                type='number'
+                value={year}
+                onChange={handleYearChange}
+                name="year"
+                placeholder='YYYY' />
+            </div>
+            </div>
+            <div className="bg-Purple   cursor-pointer self-center mt-10 h-12 w-12 rounded-full flex justify-center items-center">
+              <img className="h-7 " src={iconArrow} alt='arrow icon' />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
   )
 }
 
