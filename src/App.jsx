319b7add394690971ba754cc168ca51d149e20fc
badr ghanbi <bghanbi50@gmail.com>
2023-05-11 12:08:53 +0100

    calculate the yeas

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,26 @@ const App = () => {
   const [day, setDay] = useState('30');
   const [month, setMonth] = useState('10');
   const [year, setYear] = useState('1999');
+  const [calculatedYear, setCalculatedYear] = useState('');
+  const [calculatedMonth, setCalculatedMonth] = useState('');
+  const [calculatedDay, setCalculatedDay] = useState('');
+
 
   const handleDayChange = (e) => setDay(e.target.value)
   const handleMonthChange = (e) => setMonth(e.target.value)
   const handleYearChange = (e) => setYear(e.target.value)
 
+  const calcAge=() =>{
+   const currentTime= new Date();
+   console.log(currentTime.getFullYear())
+   setCalculatedYear(currentTime.getFullYear() - year)
+  }
   return (
     <div className='h-screen  flex justify-center items-center   bg-Offwhite' >
-        <div className='bg-White w-[70%] h-[400px] p-4 lg:w-1/2  '>
-          {/* header */}
-          <form className='flex flex-col justify-between '>
-      <div className="flex items-center justify-between">
+      <div className='bg-White w-[70%] h-[400px] p-4 lg:w-1/2  '>
+        {/* header */}
+        <form className='flex flex-col justify-between '>
+          <div className="flex items-center justify-between">
             <div>
               <label htmlFor="day">day</label>
               <input id="day"
@@ -28,7 +37,7 @@ const App = () => {
             <div>
               <label htmlFor="month" >month</label>
               <input id="month"
-              className="appearance-none"
+                className="appearance-none"
                 type='number'
                 value={month}
                 onChange={handleMonthChange}
@@ -44,47 +53,48 @@ const App = () => {
                 name="year"
                 placeholder='YYYY' />
             </div>
-            </div>
-             {/* <div className="  h-2  border-2 border-t-Purple" >
-
-            </div>  */}
-             <svg className="relative top-16 " width="100%" height="1">
-      <line x1="0" y1="0" x2="100%" y2="0" stroke="black" strokeWidth="1" />
-    </svg>
-            <div className="bg-Purple  z-20 cursor-pointer self-center mt-10 h-12 w-12 rounded-full flex justify-center items-center">
-              <img className="h-7 " src={iconArrow} alt='arrow icon' />
-            </div>
-          </form>
+          </div>
+         
+          <svg className="relative top-16 " width="100%" height="1">
+            <line x1="0" y1="0" x2="100%" y2="0" stroke="black" strokeWidth="1" />
+          </svg>
+          <div onClick={calcAge} className="bg-Purple  z-20 cursor-pointer self-center mt-10 h-12 w-12 rounded-full flex justify-center items-center">
+            <img className="h-7 " src={iconArrow} alt='arrow icon' />
+          </div>
+        </form>
         {/* date */}
-      <div className="flex mt-4  items-center">
-        <div className="flex">
-          <span className="h-2 mx-2 w-8 bg-Purple block">
-          </span>
-          <span className="h-2  w-8 mr-4 bg-Purple block">
-          </span>
+        <div className="flex mt-4  items-center">
+            {calculatedYear && <h2 className="text-Purple font-extrabold text-5xl mx-2">{calculatedYear}</h2>}
+            {!calculatedYear &&  <div className="flex">
+             <span className="h-2 mx-2 w-8 bg-Purple block">
+            </span>
+            <span className="h-2  w-8 mr-4 bg-Purple block">
+            </span>
+              </div> }
+          <h1>years</h1>
         </div>
-        <h1>years</h1>
-      </div>
-      <div className="flex mt-4  items-center">
-        <div className="flex">
-          <span className="h-2 mx-2 w-8 bg-Purple block">
-          </span>
-          <span className="h-2  w-8 mr-4 bg-Purple block">
-          </span>
+        <div className="flex mt-4  items-center">
+          {calculatedMonth && <h2 className="text-Purple font-extrabold text-5xl mx-2">{calculatedMonth}</h2>}
+          {!calculatedMonth &&  <div className="flex">
+             <span className="h-2 mx-2 w-8 bg-Purple block">
+            </span>
+            <span className="h-2  w-8 mr-4 bg-Purple block">
+            </span>
+              </div> }
+          <h1>months</h1>
         </div>
-        <h1>months</h1>
-      </div>
-      <div className="flex mt-4  items-center">
-        <div className="flex">
-          <span className="h-2 mx-2 w-8 bg-Purple block">
-          </span>
-          <span className="h-2  w-8 mr-4 bg-Purple block">
-          </span>
-        </div>
-        <h1>days</h1>
-      </div>
+        <div className="flex mt-4  items-center">
+          {calculatedDay && <h2 className="text-Purple font-extrabold text-5xl mx-2">{calculatedDay}</h2>}
+             {!calculatedDay &&  <div className="flex">
+             <span className="h-2 mx-2 w-8 bg-Purple block">
+            </span>
+            <span className="h-2  w-8 mr-4 bg-Purple block">
+            </span>
+              </div> }
+          <h1>days</h1>
         </div>
       </div>
+    </div>
   )
 }
 
